26a67feece6c244f6b6ef460e338310ebe0eb249
Takashi Kawachi <tkawachi@gmail.com>
2021-01-30 22:31:56 +0900

    update

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -1,6 +1,34 @@
-use std::env;
+use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    #[clap(short, long)]
+    verbose: bool,
+
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args)
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    } else {
+        println!("No file is specified");
+    }
 }
